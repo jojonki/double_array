5fb47154b05597e0936d83256aabbc4b5e9bc083
jojonki <junki.ohmura@gmail.com>
2019-02-28 23:10:12 +0900

    add comments

--- main.py
+++ main.py
@@ -37,18 +37,21 @@ class DoubleArray:
         print('c: {}'.format(self.check[1:]))
 
     def search(self, word):
-        if not word or type(word) != str:
-            return False, None
+        '''Search word in the double array
+
+        Rerutn search result (bool) with err
+        '''
+        if not word or type(word) != str: # unsafe return values
+            return False, None, None
 
         s = 1 # start node
         for c_ind, c in enumerate(word):
-            print('search', c, ', current', s)
             next_node = self.base[s] + code[c]
             if self.check[next_node] == s:
-                print('check ok. move to:', next_node)
+                # check ok. move to:', next_node
                 s = next_node
             else:
-                print('search fail at', c, 'in', word)
+                # 'search fail at `c` in `word`
                 return False, s, c
 
         return True, None, None
@@ -59,51 +62,53 @@ class DoubleArray:
         if ret:
             print('success')
         else:
-            if self.base[s] == 0: # not registered
+            if self.base[s] == 0: # Not used node
+                # Search empty check node
                 for ind in range(1 + code[c], self.N):
                     if self.check[ind] == 0:
                         self.base[s] = ind - code[c]
                         self.check[ind] = s
                         break
+                # TODO Handle if theare are no empty check
             else:
-                if self.check[self.base[s] + code[c]] == 0:
+                if self.check[self.base[s] + code[c]] == 0: # if there is an availabe check
                     self.check[self.base[s] + code[c]] = s
-                else:
-                    # 衝突発生ノードを親に持つノードのindexを取得
+                else: # conflict case
+                    # Gather all children nodes whose parent is the conflict node
                     child_node_list = [ch_i for ch_i, ch_v in enumerate(self.check) if ch_v == s]
-                    # 衝突発生ノードを親に持つノードのコード値を取得
+                    # Gather all children values whose parent is the conflict node
                     child_code_list = [ch_n - self.base[s] for ch_n in child_node_list]
-                    # 新たに追加しようとしている文字コード値も子ノードになるため追加
+                    # The parent of the new adding node will be the conflict node
                     child_code_list += [code[c]] # also add the char of the new vocab
-                    # 子ノードが利用できる空のcheck要素を探す
+                    # Search new empty check for the children
                     for i in range(1, self.N):
+                        # Check availability of check for the children
                         if sum([self.check[i + code_v] for code_v in child_code_list]) == 0:
-                            x = i
-                            org_base = self.base[s]
-                            self.base[s] = x
-                            # Update check of the children
-                            # for code_v in child_code_list:
-                            #     self.check[x + v2c[code_v]] = s
-                                # copy new base for the new destination
+                            offset = i
+                            org_base = self.base[s] # save the old offset
+                            self.base[s] = offset
+                            # Update the node and check of the all children with the new offset
                             for node in child_node_list:
                                 code_v = node - org_base
                                 prev_dst_node = org_base + code_v
-                                new_dst_node = x + code_v
+                                new_dst_node = offset + code_v
                                 self.base[new_dst_node] = self.base[prev_dst_node]
                                 self.check[new_dst_node] = s
 
-                                # 元遷移先のノードを親とするノード一覧のcheckも更新
+                                # Update children whose parent is the updated node, i.e., the grand parent is the conflict node
                                 for j in range(1, self.N):
                                     if self.check[j] == prev_dst_node:
                                         self.check[j] = new_dst_node
 
-                                # 元遷移先をクリア
+                                # Clear old information of the child
                                 self.base[prev_dst_node] = 0
                                 self.check[prev_dst_node] = 0
-                            # 新しく追加する文字コード値のノードのcheckもセット
-                            self.check[x + code[c]] = s
+
+                            # Set check for the new character
+                            self.check[offset + code[c]] = s
                             self.report()
                             break
+                    # TODO handle if there are no empty check
             self._build(vocab)
 
     def build(self, vocab_list):
@@ -116,7 +121,7 @@ class DoubleArray:
 
 
 def main():
-    vocab_list = ['ab#', 'abc#']
+    # vocab_list = ['ab#', 'abc#']
     vocab_list = ['ab#', 'abc#', 'ac#']
     # vocab_list = ['abc#']
     # vocab_list = ['ac#']
