aa54ee774addc8a907a536868abb635b4243724c
jojonki <junki.ohmura@gmail.com>
2019-03-03 21:11:24 +0900

    refacto

--- double_array.py
+++ double_array.py
@@ -36,10 +36,12 @@ class DoubleArray:
         print('c: {}'.format(self.check[1:]))
 
     def search(self, word):
-        '''Search word in the double array
+        """Search a word in the double array
 
-        Rerutn search result (bool) with err
-        '''
+        Rerutns:
+            Return the search result with information
+            (found: boolean, final_node: int, final_chara: str)
+        """
         if not word or type(word) != str: # unsafe return values
             return False, None, None
 
@@ -55,75 +57,98 @@ class DoubleArray:
 
         return True, None, None
 
+    def _registerVocab(self, s, c):
+        """
+        """
+        if self.base[s] == 0: # Not used based node
+            # Search empty check node
+            for ind in range(1 + self.code[c], self.N):
+                if self.check[ind] == 0:
+                    self.base[s] = ind - self.code[c]
+                    self.check[ind] = s
+                    break
+            # TODO Handle if theare are no empty check
+        else: # Used base node
+            if self.check[self.base[s] + self.code[c]] == 0: # if there is an availabe check
+                self.check[self.base[s] + self.code[c]] = s
+            else: # node conflicted
+                # Re-assign base and check nodes
+                # Gather all children nodes whose parent is the conflict node
+                child_node_list = [ch_i for ch_i, ch_v in enumerate(self.check) if ch_v == s]
+                # Gather all children values whose parent is the conflict node
+                child_code_list = [ch_n - self.base[s] for ch_n in child_node_list]
+                # The parent of the new adding node will be the conflict node
+                child_code_list += [self.code[c]] # also add the char of the new vocab
+                # Search new empty check for the children
+                for i in range(1, self.N):
+                    # Check availability of check for the children
+                    if sum([self.check[i + code_v] for code_v in child_code_list]) == 0:
+                        offset = i
+                        org_base = self.base[s] # save the old offset
+                        self.base[s] = offset
+                        # Update the node and check of the all children with the new offset
+                        for node in child_node_list:
+                            code_v = node - org_base
+                            prev_dst_node = org_base + code_v
+                            new_dst_node = offset + code_v
+                            self.base[new_dst_node] = self.base[prev_dst_node]
+                            self.check[new_dst_node] = s
+
+                            # Update children whose parent is the updated node, i.e., the grand parent is the conflict node
+                            for j in range(1, self.N):
+                                if self.check[j] == prev_dst_node:
+                                    self.check[j] = new_dst_node
+
+                            # Clear old information of the child
+                            self.base[prev_dst_node] = 0
+                            self.check[prev_dst_node] = 0
+
+                        # Set check for the new character
+                        self.check[offset + self.code[c]] = s
+                        self.report()
+                        break
+                # TODO handle if there are no empty check
 
     def _build(self, vocab):
         ret, s, c = self.search(vocab) # bool, final_node, final_char
         if ret:
             print('success')
         else:
-            if self.base[s] == 0: # Not used node
-                # Search empty check node
-                for ind in range(1 + self.code[c], self.N):
-                    if self.check[ind] == 0:
-                        self.base[s] = ind - self.code[c]
-                        self.check[ind] = s
-                        break
-                # TODO Handle if theare are no empty check
-            else:
-                if self.check[self.base[s] + self.code[c]] == 0: # if there is an availabe check
-                    self.check[self.base[s] + self.code[c]] = s
-                else: # conflict case
-                    # Gather all children nodes whose parent is the conflict node
-                    child_node_list = [ch_i for ch_i, ch_v in enumerate(self.check) if ch_v == s]
-                    # Gather all children values whose parent is the conflict node
-                    child_code_list = [ch_n - self.base[s] for ch_n in child_node_list]
-                    # The parent of the new adding node will be the conflict node
-                    child_code_list += [self.code[c]] # also add the char of the new vocab
-                    # Search new empty check for the children
-                    for i in range(1, self.N):
-                        # Check availability of check for the children
-                        if sum([self.check[i + code_v] for code_v in child_code_list]) == 0:
-                            offset = i
-                            org_base = self.base[s] # save the old offset
-                            self.base[s] = offset
-                            # Update the node and check of the all children with the new offset
-                            for node in child_node_list:
-                                code_v = node - org_base
-                                prev_dst_node = org_base + code_v
-                                new_dst_node = offset + code_v
-                                self.base[new_dst_node] = self.base[prev_dst_node]
-                                self.check[new_dst_node] = s
-
-                                # Update children whose parent is the updated node, i.e., the grand parent is the conflict node
-                                for j in range(1, self.N):
-                                    if self.check[j] == prev_dst_node:
-                                        self.check[j] = new_dst_node
-
-                                # Clear old information of the child
-                                self.base[prev_dst_node] = 0
-                                self.check[prev_dst_node] = 0
-
-                            # Set check for the new character
-                            self.check[offset + self.code[c]] = s
-                            self.report()
-                            break
-                    # TODO handle if there are no empty check
-            self._build(vocab)
+            self._registerVocab(s, c)
+            self._build(vocab) # TODO(jonki) recursive call should be handled safely
 
     def build(self, vocab_list):
+        """Build an double array from a vocabulary list
+
+        Args:
+            vocab_list: An array of vocabuary(str)
+
+        Returns:
+            A boolean indicating if it succeed to build the dictioanry or not.
+        """
         for vocab in vocab_list:
             print('vocab', vocab)
             self._build(vocab)
 
-    def commonPrefixSearch(self):
+    def commonPrefixSearch(self, input_str):
+        """Search all common prefix from the dictionary
+
+        Args:
+            input_str: A sentence of the query.
+
+        Return:
+            prefix_list: A list of words contains input_str as prefix
+        """
         pass
 
     def save(self, fpath, base, check):
+        """Save a double array to a file"""
         with open(fpath, 'w') as fout:
             fout.write('{}\n'.format(','.join(base)))
             fout.write('{}\n'.format(','.join(check)))
 
     def load(self, fpath):
+        """Load a file of a double array"""
         ret = False
         if os.path.exists(fpath):
             with open(fpath, 'r') as fin:
