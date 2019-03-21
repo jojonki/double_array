6fd6e1c10402607d1cdeb2077510bca6c4f05d0e
jojonki <junki.ohmura@gmail.com>
2019-03-21 23:05:07 +0900

    add arg of dict

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ def build(da, vocab_file):
     print('Building vocabuary...')
     da.build(word_list)
     print('Built!')
+    da.save('./models/dict')
     da.report()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--vocab', type=str, metavar='PATH', required=True, help='vocabulary file')
+    parser.add_argument('--dict', type=str, metavar='PATH', help='build double array')
     args = parser.parse_args()
 
     da = DoubleArray()
-    build(da, args.vocab)
+    if args.dict:
+        da.load(args.dict)
+    else:
+        build(da, args.vocab)
+        # da.report(verbose=True)
 
 
     query_list = ['東', '東京', '東京タワー', '東京都議会', '加藤清正']
