bff3242b55f417ee79fae53f1bc14078dd590f4f
jojonki <junki.ohmura@gmail.com>
2019-03-28 22:52:28 +0900

    do not use property N

--- double_array.py
+++ double_array.py
@@ -12,19 +12,6 @@ class DoubleArray:
         self.clear()
 
         self._left = 1
-        
-    @property
-    def base(self):
-        return self._base
-
-    @property
-    def check(self):
-        return self._check
-
-    @property
-    def N(self):
-        assert len(self._base) == len(self._check)
-        return len(self._base)
 
     def _expand(self, diff):
         """Expand double array"""
@@ -37,7 +24,8 @@ class DoubleArray:
         self._check = [0] * self._data_size
 
     def report(self, verbose=False):
-        print('Array length: {}'.format(self.N))
+        N = len(self._base)
+        print('Array length: {}'.format(N))
         d_size = (sys.getsizeof(self._base) + sys.getsizeof(self._check))
         unit = 'B'
         if d_size > 1024:
@@ -48,9 +36,9 @@ class DoubleArray:
             d_size /= 1024
         print('Double-Array size: {:.1f} {}'.format(d_size, unit))
         if verbose:
-            print('i:  {}'.format(', '.join([str(i) for i in range(1, self.N)])))
-            print('b: {}'.format(self.base[1:]))
-            print('c: {}'.format(self.check[1:]))
+            print('i:  {}'.format(', '.join([str(i) for i in range(1, N)])))
+            print('b: {}'.format(self._base[1:]))
+            print('c: {}'.format(self._check[1:]))
 
     def search(self, word, start_node=1):
         """Search a word in the double array
@@ -62,8 +50,8 @@ class DoubleArray:
         crnt_node = start_node
         crnt_char = None
         for c_ind, c in enumerate(word):
-            next_node = self.base[crnt_node] + c
-            if next_node < len(self.check) and self.check[next_node] == crnt_node:
+            next_node = self._base[crnt_node] + c
+            if next_node < len(self._check) and self._check[next_node] == crnt_node:
                 # check ok. move to:', next_node
                 crnt_node = next_node
                 crnt_char = c
@@ -76,34 +64,35 @@ class DoubleArray:
     def _registerVocab(self, s, c):
         """
         """
-        if self.base[s] == 0: # Not used based node
+        if self._base[s] == 0: # Not used based node
             # Search an empty check node
             try:
                 dst_check_index = 1 + c + self._check[1+c:].index(0)
             except ValueError as e: # No empty check nodes
                 # Expand check
                 self._expand(1)
-                dst_check_index = self.N - 1
+                dst_check_index = len(self._base) - 1
 
-            self.base[s] = dst_check_index - c
-            self.check[dst_check_index] = s
+            self._base[s] = dst_check_index - c
+            self._check[dst_check_index] = s
             s = dst_check_index # move to next node
         else: # Used base node
-            if (self.base[s] + c < self.N) and self.check[self.base[s] + c] == 0: # if check is correct
-                self.check[self.base[s] + c] = s
-                s = self.base[s] + c # move to next node
+            # N = len(self._base)
+            if (self._base[s] + c < len(self._base)) and self._check[self._base[s] + c] == 0: # if check is correct
+                self._check[self._base[s] + c] = s
+                s = self._base[s] + c # move to next node
             else: # node conflicted
                 # Re-assign base and check nodes
                 # Gather all children nodes whose parent is the conflict node
                 # Gather all children values whose parent is the conflict node
                 # The parent of the new adding node will be the conflict node
                 child_node_list, child_code_list = [], []
-                for ch_i, ch_v in enumerate(self.check):
+                for ch_i, ch_v in enumerate(self._check):
                     if ch_v == s:
                         child_node_list.append(ch_i)
-                        child_code_list.append(ch_i - self.base[s])
+                        child_code_list.append(ch_i - self._base[s])
                 child_code_list += [c]
-                for i in range(self._left, self.N):
+                for i in range(self._left, len(self._base)):
                     # Search new empty check for the children
                     found_empty_check = True
 
@@ -114,7 +103,7 @@ class DoubleArray:
 
                     # All check must be 0
                     for code_v in child_code_list:
-                        if self.check[i + code_v] != 0:
+                        if self._check[i + code_v] != 0:
                             found_empty_check = False
                             break
 
@@ -122,32 +111,32 @@ class DoubleArray:
                         continue
 
                     # FOUND an empty node
-                    if self.check[self._left:i].count(0) < (i - self._left + 1) * 0.05:
+                    if self._check[self._left:i].count(0) < (i - self._left + 1) * 0.05:
                         # print('$$$$$$$$$$$$$$$ update left {} -> {}'.format(self._left, i))
                         self._left = i
 
                     offset = i
-                    org_base = self.base[s] # save the old offset
-                    self.base[s] = offset
+                    org_base = self._base[s] # save the old offset
+                    self._base[s] = offset
                     # Update the node and check of the all children with the new offset
                     for node in child_node_list:
                         code_v = node - org_base
                         prev_dst_node = org_base + code_v
                         new_dst_node = offset + code_v
-                        self.base[new_dst_node] = self.base[prev_dst_node]
-                        self.check[new_dst_node] = s
+                        self._base[new_dst_node] = self._base[prev_dst_node]
+                        self._check[new_dst_node] = s
 
                         # Update children whose parent is the updated node, i.e., the grand parent is the conflict node
-                        for j in range(1, self.N):
-                            if self.check[j] == prev_dst_node:
-                                self.check[j] = new_dst_node
+                        for j in range(1, len(self._base)):
+                            if self._check[j] == prev_dst_node:
+                                self._check[j] = new_dst_node
 
                         # Clear old information of the child
-                        self.base[prev_dst_node] = 0
-                        self.check[prev_dst_node] = 0
+                        self._base[prev_dst_node] = 0
+                        self._check[prev_dst_node] = 0
 
                     # Set check for the new character
-                    self.check[offset + c] = s
+                    self._check[offset + c] = s
                     # self.report()
                     s = offset + c # move to next node
                     break
@@ -182,7 +171,7 @@ class DoubleArray:
             if not vocab.endswith(T):
                 vocab += T
             self._build(vocab)
-            self.report()
+        self.report()
 
     def commonPrefixSearch(self, input_str):
         """Search all common prefix of input string from the dictionary.
@@ -198,8 +187,8 @@ class DoubleArray:
         for ind, char in enumerate(input_str, 1):
             byte_char = char.encode('utf-8')
             ret, final_node, final_char, char_ind = self.search(byte_char, start_node=final_node)
-            # if ret and self.check[self.base[final_node] + T] == final_node:
-            if ret and self.check[self.base[final_node] + 35] == final_node: # '#' -> 35
+            # if ret and self._check[self._base[final_node] + T] == final_node:
+            if ret and self._check[self._base[final_node] + 35] == final_node: # '#' -> 35
                 print('"{}" found in the dictionary'.format(input_str[:ind]))
                 cp_list.append(input_str[:ind])
             else:
@@ -211,8 +200,8 @@ class DoubleArray:
     def save(self, fpath):
         """Save a double array to a file"""
         with open(fpath, 'w') as fout:
-            fout.write('{}\n'.format(','.join([str(ind) for ind in self.base])))
-            fout.write('{}\n'.format(','.join([str(ind) for ind in self.check])))
+            fout.write('{}\n'.format(','.join([str(ind) for ind in self._base])))
+            fout.write('{}\n'.format(','.join([str(ind) for ind in self._check])))
 
     def load(self, fpath):
         """Load a file of a double array"""
