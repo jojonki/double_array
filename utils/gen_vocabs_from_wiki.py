427b31013d2fa9bf24895c5cce2940e917f566b2
jojonki <junki.ohmura@gmail.com>
2019-04-14 15:23:24 +0900

    rename

--- utils/gen_vocabs_from_wiki.py
+++ utils/gen_vocabs_from_wiki.py
@@ -13,9 +13,12 @@ random.seed(1111)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--N', type=int, help='Number of samples. Use all lines if this is not specified')
+    parser.add_argument('--wiki', type=str, metavar='PATH',
+                            default='./data/jawiki-latest-all-titles-in-ns0.gz',
+                            help='wikipedia all titles file')
     parser.add_argument('--out', type=str, metavar='PATH', required=True, help='output file path')
     args = parser.parse_args()
-    with gzip.open('data/jawiki-latest-all-titles-in-ns0.gz', 'r') as fin:
+    with gzip.open(args.wiki, 'r') as fin:
         with open(args.out, 'w', encoding='utf-8') as fout:
             if args.N:
                 lines = random.sample(fin.readlines(), args.N)
